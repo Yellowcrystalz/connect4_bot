46a3fa6c43c4171952bab733da392c30cae74cac
Yellowcrystalz <jaykaycrystal0914@gmail.com>
2024-08-07 18:22:17 -0500

    Added reactions

--- main.py
+++ main.py
@@ -16,7 +16,16 @@ async def on_ready():
 @bot.command()
 async def play(ctx):
     grid = discord.Embed(title="Yellow's Connect 4", color=discord.Color.yellow())
-    grid.add_field(name="", value=":black_large_square:")
-    await ctx.send(embed=grid)
+    grid.add_field(name="", value=":white_square_button: :white_square_button: :white_square_button: :white_square_button: :white_square_button: :white_square_button: :white_square_button:\n:white_square_button: :white_square_button: :white_square_button: :white_square_button: :white_square_button: :white_square_button: :white_square_button:\n:white_square_button: :white_square_button: :white_square_button: :white_square_button: :white_square_button: :white_square_button: :white_square_button:\n:white_square_button: :white_square_button: :white_square_button: :white_square_button: :white_square_button: :white_square_button: :white_square_button:\n:white_square_button: :white_square_button: :white_square_button: :white_square_button: :white_square_button: :white_square_button: :white_square_button:\n:white_square_button: :white_square_button: :white_square_button: :white_square_button: :white_square_button: :white_square_button: :white_square_button:")
+    
+    message = await ctx.send(embed=grid)
+
+    await message.add_reaction('\u0031\uFE0F\u20E3')
+    await message.add_reaction('\u0032\uFE0F\u20E3')
+    await message.add_reaction('\u0033\uFE0F\u20E3')
+    await message.add_reaction('\u0034\uFE0F\u20E3')
+    await message.add_reaction('\u0035\uFE0F\u20E3')
+    await message.add_reaction('\u0036\uFE0F\u20E3')
+    await message.add_reaction('\u0037\uFE0F\u20E3')
 
 bot.run(token)
