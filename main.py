370c262039f57bbe1590f3dcee0a2d1cf6c453f4
Yellowcrystalz <jaykaycrystal0914@gmail.com>
2024-08-08 01:34:18 -0500

    some functionality

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import discord
 from discord.ext import commands
 import os
 from dotenv import load_dotenv
+import asyncio
 
 
 load_dotenv()
@@ -14,18 +15,19 @@ async def on_ready():
     print("Online")
 
 @bot.command()
-async def play(ctx):
-    grid = discord.Embed(title="Yellow's Connect 4", color=discord.Color.yellow())
-    grid.add_field(name="", value=":white_square_button: :white_square_button: :white_square_button: :white_square_button: :white_square_button: :white_square_button: :white_square_button:\n:white_square_button: :white_square_button: :white_square_button: :white_square_button: :white_square_button: :white_square_button: :white_square_button:\n:white_square_button: :white_square_button: :white_square_button: :white_square_button: :white_square_button: :white_square_button: :white_square_button:\n:white_square_button: :white_square_button: :white_square_button: :white_square_button: :white_square_button: :white_square_button: :white_square_button:\n:white_square_button: :white_square_button: :white_square_button: :white_square_button: :white_square_button: :white_square_button: :white_square_button:\n:white_square_button: :white_square_button: :white_square_button: :white_square_button: :white_square_button: :white_square_button: :white_square_button:")
-    
-    message = await ctx.send(embed=grid)
+@commands.is_owner()
+async def shutdown(ctx):
+    await ctx.send("I am dying")
+    await bot.close()
 
-    await message.add_reaction('\u0031\uFE0F\u20E3')
-    await message.add_reaction('\u0032\uFE0F\u20E3')
-    await message.add_reaction('\u0033\uFE0F\u20E3')
-    await message.add_reaction('\u0034\uFE0F\u20E3')
-    await message.add_reaction('\u0035\uFE0F\u20E3')
-    await message.add_reaction('\u0036\uFE0F\u20E3')
-    await message.add_reaction('\u0037\uFE0F\u20E3')
+async def load():
+    for filename in os.listdir('./cogs'):
+        if filename.endswith('.py'):
+            await bot.load_extension(f'cogs.{filename[:-3]}')
 
-bot.run(token)
+async def main():
+    async with bot:
+        await load()
+        await bot.start(token)
+
+asyncio.run(main())
