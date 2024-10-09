f163acad666bcfd94b2c2e321a56c122eee026cc
yellowcrystalz <jaykaycrystal0914@gmail.com>
2024-10-09 02:24:52 -0500

    fix(database): changed sql to sqlite syntax

--- python/database/connect4_schema.sql
+++ python/database/connect4_schema.sql
@@ -2,23 +2,23 @@ DROP TABLE IF EXISTS Match;
 DROP TABLE IF EXISTS Player;
 
 CREATE TABLE Player (
-    P_ID    CHAR(18),
-    P_Win   NUMBER,
-    P_Lose  NUMBER,
-    P_Draw  NUMBER,
+    P_ID    TEXT,
+    P_Win   INTEGER,
+    P_Lose  INTEGER,
+    P_Draw  INTEGER,
 
-    CONSTRAINT PlayerPK PRIMARY KEY(P_ID)
+    PRIMARY KEY(P_ID)
 );
 
 CREATE TABLE Match (
-    M_ID                NUMBER,
-    M_RedPlayerID       CHAR(18),
-    M_YellowPlayerID    CHAR(18),
-    M_Sequence          VARCHAR(42),
-    M_WinPlayer         CHAR(1),
-    M_Date              DATE, 
+    M_ID                INTEGER,
+    M_RedPlayerID       TEXT,
+    M_YellowPlayerID    TEXT,
+    M_Sequence          TEXT,
+    M_WinPlayer         TEXT,
+    M_Date              TEXT, 
 
-    CONSTRAINT MatchPK PRIMARY KEY(M_ID),
-    CONSTRAINT M_RedPlayerFK FOREIGN KEY(M_RedPlayerID) REFERENCES Player(P_ID),
-    CONSTRAINT M_YellowPlayerFK FOREIGN KEY(M_YellowPlayerID) REFERENCES Player(P_ID)
+    PRIMARY KEY(M_ID),
+    FOREIGN KEY(M_RedPlayerID) REFERENCES Player(P_ID),
+    FOREIGN KEY(M_YellowPlayerID) REFERENCES Player(P_ID)
 );
